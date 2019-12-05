747d266813052fa21b6a862ac8edd8a1d3417594
atomwoww <ojs0106@gmail.com>
2019-12-05 14:53:50 +0000

    Updates

---
+++ precache-manifest.b0c5a4b2dee10d99c703bf89c759803a.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "defbb5912484b376c75f8235e1830b4c",
+    "url": "/frontend/static/media/NoirPro-Light.defbb591.eot"
+  },
+  {
+    "revision": "910b3116ebad19616c70",
+    "url": "/frontend/static/css/main.f3825aac.chunk.css"
+  },
+  {
+    "revision": "09db1c344603df3bb8be",
+    "url": "/frontend/static/js/runtime~main.d60cc9b3.js"
+  },
+  {
+    "revision": "9dbcc95df5c86335f0e083c353c51239",
+    "url": "/frontend/static/media/NoirPro-Bold.9dbcc95d.woff"
+  },
+  {
+    "revision": "b9b0ae5be6f5c9ecab2b",
+    "url": "/frontend/static/js/2.b721ad5a.chunk.js"
+  },
+  {
+    "revision": "c656976ef38c08d0e164",
+    "url": "/frontend/static/js/3.c65bf43b.chunk.js"
+  },
+  {
+    "revision": "74caa41d794fb97ca5480b4ad0a2db03",
+    "url": "/frontend/static/media/loader.74caa41d.svg"
+  },
+  {
+    "revision": "7ed12ca430db89e02f473502648eadc5",
+    "url": "/frontend/static/media/sprite-flags-24x24.7ed12ca4.png"
+  },
+  {
+    "revision": "36251c7bf6634a7cd87cf02935463f61",
+    "url": "/frontend/static/media/gaxon.36251c7b.woff"
+  },
+  {
+    "revision": "00fcca8e4896ea70aeaf79cc01482b54",
+    "url": "/frontend/static/media/gaxon.00fcca8e.ttf"
+  },
+  {
+    "revision": "32cebc42775933015139f56793bcdca8",
+    "url": "/frontend/static/media/gaxon.32cebc42.eot"
+  },
+  {
+    "revision": "b1a63de8de332478102c86d8481bafc2",
+    "url": "/frontend/static/media/gaxon.b1a63de8.svg"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/frontend/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/frontend/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/frontend/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/frontend/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "752c13e1b881564f5f54afa1a4859492",
+    "url": "/frontend/static/media/NoirPro-Regular.752c13e1.eot"
+  },
+  {
+    "revision": "910b3116ebad19616c70",
+    "url": "/frontend/static/js/main.7150facc.chunk.js"
+  },
+  {
+    "revision": "fe70663122656a5c59de60232d8be61f",
+    "url": "/frontend/static/media/NoirPro-Regular.fe706631.woff2"
+  },
+  {
+    "revision": "251883125165605d8c409595ff236734",
+    "url": "/frontend/static/media/NoirPro-Light.25188312.woff2"
+  },
+  {
+    "revision": "3d82e791b707535b54dd0d68ba38cdbc",
+    "url": "/frontend/static/media/NoirPro-Medium.3d82e791.woff2"
+  },
+  {
+    "revision": "e928d2a22faf75b6884fd3b4763bdcee",
+    "url": "/frontend/static/media/NoirPro-SemiBold.e928d2a2.woff2"
+  },
+  {
+    "revision": "bb9d0cc624e14a907449e5ea1f602186",
+    "url": "/frontend/static/media/NoirPro-Bold.bb9d0cc6.woff2"
+  },
+  {
+    "revision": "873c8fa8f69d6a2118f1119e1e80eb74",
+    "url": "/frontend/static/media/NoirPro-Heavy.873c8fa8.woff2"
+  },
+  {
+    "revision": "bbd56c69aef4c37772e2305a2c2cd422",
+    "url": "/frontend/static/media/NoirPro-Light.bbd56c69.woff"
+  },
+  {
+    "revision": "83b986718958eae204ae595909a4d66e",
+    "url": "/frontend/static/media/NoirPro-Regular.83b98671.woff"
+  },
+  {
+    "revision": "6d74487df6dc4e67a2f4c3fd8650362c",
+    "url": "/frontend/static/media/NoirPro-Medium.6d74487d.eot"
+  },
+  {
+    "revision": "7194b3b5891959a3e98285e89bdc01c6",
+    "url": "/frontend/static/media/NoirPro-Heavy.7194b3b5.eot"
+  },
+  {
+    "revision": "d956581657d758fdda26a51e591a8a85",
+    "url": "/frontend/static/media/NoirPro-Bold.d9565816.eot"
+  },
+  {
+    "revision": "ac97525f7e84a8b62907058d6aa4e93c",
+    "url": "/frontend/static/media/NoirPro-SemiBold.ac97525f.eot"
+  },
+  {
+    "revision": "1bc66a4b5cd48c729a854842fd066a1b",
+    "url": "/frontend/static/media/NoirPro-SemiBold.1bc66a4b.woff"
+  },
+  {
+    "revision": "04b5d842dfad0fbb345cda0473c72d0e",
+    "url": "/frontend/static/media/NoirPro-Medium.04b5d842.woff"
+  },
+  {
+    "revision": "a302e546bfe8cef2a36504d844542fa4",
+    "url": "/frontend/static/media/NoirPro-Heavy.a302e546.woff"
+  },
+  {
+    "revision": "b9b0ae5be6f5c9ecab2b",
+    "url": "/frontend/static/css/2.18308bf9.chunk.css"
+  },
+  {
+    "revision": "faee79af0f30d4ca745749164e309172",
+    "url": "/frontend/index.html"
+  }
+];
